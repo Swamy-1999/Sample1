5db751d9b57fde6b360e7ef094e4627b9600d9f3
B.ARAVINDA SWAMY <78960851+Swamy-1999@users.noreply.github.com>
2025-08-06 18:26:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -13,5 +13,5 @@ def sub_number(c, d):
 if __name__ == "__main__":
     num3 = float(input("Enter first number: "))
     num4 = float(input("Enter second number: "))
-    result2 = add_numbers(num1, num2)
+    result2 = add_numbers(num3, num4)
     print(f"The sub is: {result2}")
